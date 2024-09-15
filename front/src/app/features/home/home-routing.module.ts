9c1cbeec727f7552f584ba81cd12b207c43f7a6b
Zakaria Fahraoui <zakaria.fahraoui@gmail.com>
2024-09-16 00:26:52 +0200

    add user info

--- front/src/app/features/home/home-routing.module.ts
+++ front/src/app/features/home/home-routing.module.ts
@@ -1,20 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
-import { ReservationDetailsComponent } from '../reservations/reservation-details/reservation-details.component';
-import { ReservationResolver } from '../reservations/reservation.resolver';
 
 const routes: Routes = [
   { 
     path: '', 
     component: HomeComponent 
-  },
-  { 
-    path: 'reservation/:id', 
-    component: ReservationDetailsComponent,
-    resolve: {
-      reservation: ReservationResolver
-    }
   }
 ];
 
--- front/src/app/features/reservations/reservations-routing.module.ts
+++ front/src/app/features/reservations/reservations-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ReservationsComponent } from './reservations.component';
+import { ReservationDetailsComponent } from './reservation-details/reservation-details.component';
+import { ReservationResolver } from './reservation.resolver';
 
-const routes: Routes = [{ path: '', component: ReservationsComponent }];
+const routes: Routes = [
+  { 
+    path: '',
+    component: ReservationsComponent 
+  },
+  { 
+    path: ':id', 
+    component: ReservationDetailsComponent,
+    resolve: {
+      reservation: ReservationResolver
+    }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
